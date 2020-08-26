3adebc0b3aaf16bbf85a87c8e80d4f265003280b
Mike Manchee <mikemanchee@gmail.com>
2020-08-26 09:44:22 -0700

    add rollDice function to Pig Dice project (works)
    Co-authored-by David Butler <davidabutler92@yahoo.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic Game
+// const Player = function(name, score, playerTurn) {
+//   this.name = name;
+//   this.score = score;
+//   this.playerTurn = playerTurn;
+// }
+
+// Business Logic Player
+
+// Roll Function
+const rollDice = () => {
+  let roll = 0;
+  roll = Math.ceil(Math.random() * 6);
+  console.log(roll);
+  return roll;
+}
+
+// Add Function
+
+// Hold Function
+
+// EndTurn Function 
+
+// Win Function
+
+// Reload Function
+
+// User Interface Logic
+
+$(document).ready(function() {
+  $("#")
+});
